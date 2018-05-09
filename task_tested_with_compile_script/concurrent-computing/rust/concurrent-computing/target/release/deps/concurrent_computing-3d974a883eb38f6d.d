f3223e04a88b24e1ff0d5f7ebc1dd3ebf325de2c
stefanos1316 <stefanos1316@gmail.com>
2018-05-09 19:25:33 +0300

    Fixed issue with Rust executables

---
+++ task_tested_with_compile_script/concurrent-computing/rust/concurrent-computing/target/release/deps/concurrent_computing-3d974a883eb38f6d.d
@@ -0,0 +1,5 @@
+/home/sgeorgiou/GitHub/Rosetta_Code_Data_Set/task_tested_with_compile_script/concurrent-computing/rust/concurrent-computing/target/release/deps/concurrent_computing-3d974a883eb38f6d: src/main.rs
+
+/home/sgeorgiou/GitHub/Rosetta_Code_Data_Set/task_tested_with_compile_script/concurrent-computing/rust/concurrent-computing/target/release/deps/concurrent_computing-3d974a883eb38f6d.d: src/main.rs
+
+src/main.rs:
